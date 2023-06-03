37059a84c861ea1ae46bb5aa2d52ee7bd1746da4
Jujumba <far77578@gmail.com>
2023-06-03 20:09:47 +0200

    lexer: avoid repeatable code

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,17 +1,41 @@
 pub mod tokens;
 
-use tokens::{Token, KeywordType, OperatorType};
+use tokens::{Token, KeywordType, OperatorType, LiteralType, NumType};
 
-use lazy_static::lazy_static;
 use regex::Regex;
+use lazy_static::lazy_static;
 
 use crate::input::InputFile;
 
 lazy_static!(
-    static ref KEYWORD_REGEX: Regex = Regex::new(r"[a-zA-Z0-9]+").unwrap();
-    static ref NUMBERS_REGEX: Regex = Regex::new(r"[0-9]+").unwrap();
-    static ref STRING_REGEX: Regex = Regex::new("\"[a-zA-Z0-0]+\"").unwrap();
-    static ref OPERATORS_REGEX: Regex = Regex::new(r"<<=|>>=|<=|>=|\+=|\-=|\*=|/=|\|=|\^=|&=|%=|<<|>>|=|\+|-|\*|/|%|&|\^|\||~|!|<|>").unwrap();
+    static ref SPEC: Vec<(Regex, Box<dyn Fn(&str) -> Token + Sync>)> = vec!{
+        (Regex::new(r"[0-9]+").unwrap(), Box::new(
+            |s| {
+                Token::Literal(LiteralType::Num(NumType::Int(s.parse().unwrap())))
+            }
+        )),
+        (Regex::new("\"[a-zA-Z0-0]+\"").unwrap(), Box::new(
+            |s| {
+                Token::Literal(LiteralType::Str(s.into()))
+            }
+        )),
+        (Regex::new(r"[a-zA-Z0-9]+").unwrap(), Box::new(
+            |s| {
+                match KeywordType::to_keyword(s) {
+                    Some(keyword) => Token::Keyword(keyword),
+                    None => Token::Identifier(s.into())
+                }
+            }
+        )),
+        (Regex::new(r"<<=|>>=|<=|>=|\+=|\-=|\*=|/=|\|=|\^=|&=|%=|<<|>>|=|\+|-|\*|/|%|&|\^|\||~|!|<|>").unwrap(), Box::new(
+            |s| {
+                match OperatorType::to_operator(s) {
+                    Some(operator) => Token::Operator(operator),
+                    None => Token::Undefined(s.into())
+                }
+            }
+        ))
+    };
 );
 
 pub fn next_token(input: &mut InputFile) -> Token {
@@ -19,45 +43,17 @@ pub fn next_token(input: &mut InputFile) -> Token {
         return Token::EOF;
     }
     let content = input.content.iter().collect::<String>();
-    match NUMBERS_REGEX.find_at(&content, input.cursor) {
-        Some(num) if num.start() == input.cursor => { // todo
-            let num = num.as_str();
-            input.cursor += num.len(); // todo: repeatable code
-            return Token::Literal(tokens::LiteralType::Num(tokens::NumType::Int(num.parse().unwrap())));
-        },
-        _ => ()
-    };
-    match STRING_REGEX.find_at(&content, input.cursor) {
-        Some(str) if str.start() == input.cursor => {
-            let str: String = str.as_str().into();
-            input.cursor += str.len();
-            return Token::Literal(tokens::LiteralType::Str(str));
-        }
-       _ => (),
-    }
-    match KEYWORD_REGEX.find_at(&content, input.cursor) {
-        Some(keyword) if keyword.start() == input.cursor => {
-            let keyword = keyword.as_str();
-            input.cursor += keyword.len();
-            return match KeywordType::to_keyword(keyword) {
-                Some(keyword) => Token::Keyword(keyword),
-                None => Token::Identifier(keyword.to_string())
-            };
-        }
-        _ => ()
-    }
-    match OPERATORS_REGEX.find_at(&content, input.cursor) {
-        Some(operator) if operator.start() == input.cursor => {
-            let operator = operator.as_str();
-            input.cursor += operator.len();
-            return match OperatorType::to_operator(operator) {
-                Some(operator) => Token::Operator(operator),
-                None => Token::Undefined(operator.to_string())
+    for (regex, closure) in SPEC.iter() {
+        match regex.find_at(&content, input.cursor) {
+            Some(m) if m.start() == input.cursor => {
+                let s = m.as_str();
+                input.move_cursor(s.len());
+                return closure(s);
             }
+            _ => (),
         }
-        _ => () 
     }
     let t  = Token::Undefined(input.current_char().to_string());
-    input.cursor += 1;
+    input.move_cursor(1);
     t
 }
