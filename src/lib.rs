9bb1ee12a963bfab943ef422214008976d890ba0
Jujumba <far77578@gmail.com>
2023-06-04 22:18:35 +0200

    parser: cover parsing with a test

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,17 @@ pub mod input;
 pub mod lexer;
 pub mod parser;
 pub mod ast;
+
+#[cfg(test)]
+mod tests {
+    use crate::{input, parser};
+    #[test]
+    fn test_parsing() {
+        let mut input = input::InputFile {
+            content: "1 + 1 * 2;".chars().collect(),
+            cursor: 0,
+        };
+        let mut input_clone = input.clone();
+        assert_eq!(parser::parse_expression_iter(&mut input), parser::parse_expression_recurs(&mut input_clone));
+    }
+}
