3ec54d3abd62975a4bfa4fda872963d68e3c2920
Jujumba <far77578@gmail.com>
2023-06-05 23:23:11 +0200

    ast: parser: enhancing parsing

--- src/ast/parser.rs
+++ src/ast/parser.rs
@@ -1,31 +1,41 @@
-use std::collections::VecDeque;
-
 use crate::ast::Ast;
 use crate::input::InputFile;
 use crate::lexer::next_token;
-use crate::lexer::tokens::{BinaryOperator, OperatorKind, TokenKind, KeywordKind, AssignmentOperator};
+use crate::lexer::tokens::{OperatorKind, TokenKind, KeywordKind, AssignmentOperator};
 
+pub fn parse(input: &mut InputFile) -> Result<Ast, String> {
+    let t = next_token(input);
+    match &t.kind {
+        TokenKind::Keyword(KeywordKind::Let) => parse_let_expr(input),
+        TokenKind::Identifier(_) | TokenKind::Literal(_) => {
+            input.move_back_cursor(t.len);
+            parse_expression(input)
+        }
+        _ => Err(format!("Unsupported!"))
+    }
+}
 pub fn parse_expression(input: &mut InputFile) -> Result<Ast, String> {
     let t = next_token(input);
     let ast = match t.kind {
-        TokenKind::Identifier(_) | TokenKind::Literal(_) => Ast::ValueNode(t),
+        TokenKind::Literal(_) => Ast::ValueNode(t),
+        TokenKind::Identifier(i) => Ast::IdentifierNode(i),
         e => return Err(format!("Expected identifier or literal, but got `{e:#?}")),
     };
     match next_token(input).kind {
-        TokenKind::Operator(OperatorKind::Binary(op)) => Ok(Ast::BinaryNode {
+        TokenKind::Operator(OperatorKind::Semicol) => Ok(ast),
+        TokenKind::Operator(OperatorKind::Assignment(_)) if matches!(ast, Ast::ValueNode(_)) => {
+            Err(format!("Expected identifier, but got a const-value")) // todo: informative message
+        }
+        TokenKind::Operator(op) => Ok(
+            Ast::BinaryNode {
             left: Box::new(ast),
             right: Box::new(parse_expression(input)?),
             op,
         }),
-        TokenKind::Operator(OperatorKind::Semicol) => Ok(ast),
         e => Err(format!("Expected operator or semicolon, but got `{e:#?}`")),
     }
 }
 pub fn parse_let_expr(input: &mut InputFile) -> Result<Ast, String> {
-    let t = next_token(input);
-    if TokenKind::Keyword(KeywordKind::Let) != t.kind  {
-        return Err(format!("Expected `let`, but got `{t:?}`"));
-    }
     let assignee = next_token(input);
     match &assignee.kind {
         TokenKind::Identifier(_) => {
@@ -41,39 +51,4 @@ pub fn parse_let_expr(input: &mut InputFile) -> Result<Ast, String> {
         }
         e => Err(format!("Expected identifier, but got `{e:?}`"))
     }
-}
-
-#[allow(dead_code)]
-fn parse_expression_iter(input: &mut InputFile) -> Result<Ast, String> {
-    let mut ast_stack: VecDeque<Ast> = VecDeque::new();
-    let mut op_stack: VecDeque<BinaryOperator> = VecDeque::new();
-    loop {
-        let t = next_token(input);
-        match &t.kind {
-            TokenKind::Identifier(_) | TokenKind::Literal(_) => ast_stack.push_back(Ast::ValueNode(t)),
-            e => return Err(format!("Expected identifier ot literal, but got `{e:?}`")),
-        }
-        let t = next_token(input);
-        match t.kind {
-            TokenKind::Operator(OperatorKind::Semicol) => break,
-            TokenKind::Operator(OperatorKind::Binary(bin)) => op_stack.push_back(bin),
-            e => return Err(format!("Expected operator or semicolon, but got `{e:?}`")),
-        }
-    }
-    if op_stack.is_empty() {
-        return Ok(ast_stack.pop_front().unwrap());
-    }
-    let mut ast = Ast::BinaryNode {
-        right: Box::new(ast_stack.pop_back().unwrap()),
-        left: Box::new(ast_stack.pop_back().unwrap()),
-        op: op_stack.pop_back().unwrap(),
-    };
-    while !ast_stack.is_empty() {
-        ast = Ast::BinaryNode {
-            left: Box::new(ast_stack.pop_back().unwrap()),
-            right: Box::new(ast),
-            op: op_stack.pop_back().unwrap(),
-        }
-    }
-    return Ok(ast);
 }
