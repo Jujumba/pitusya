a2fa241ed56cd69ddd6606c8f03f75e993302ccb
Jujumba <far77578@gmail.com>
2023-06-05 18:28:12 +0200

    ast: parser: move dead code

--- src/ast/parser.rs
+++ src/ast/parser.rs
@@ -5,40 +5,6 @@ use crate::input::InputFile;
 use crate::lexer::next_token;
 use crate::lexer::tokens::{BinaryOperator, OperatorKind, TokenKind, KeywordKind, AssignmentOperator};
 
-#[allow(dead_code)]
-fn parse_expression_iter(input: &mut InputFile) -> Result<Ast, String> {
-    let mut ast_stack: VecDeque<Ast> = VecDeque::new();
-    let mut op_stack: VecDeque<BinaryOperator> = VecDeque::new();
-    loop {
-        let t = next_token(input);
-        match &t.kind {
-            TokenKind::Identifier(_) | TokenKind::Literal(_) => ast_stack.push_back(Ast::ValueNode(t)),
-            e => return Err(format!("Expected identifier ot literal, but got `{e:?}`")),
-        }
-        let t = next_token(input);
-        match t.kind {
-            TokenKind::Operator(OperatorKind::Semicol) => break,
-            TokenKind::Operator(OperatorKind::Binary(bin)) => op_stack.push_back(bin),
-            e => return Err(format!("Expected operator or semicolon, but got `{e:?}`")),
-        }
-    }
-    if op_stack.is_empty() {
-        return Ok(ast_stack.pop_front().unwrap());
-    }
-    let mut ast = Ast::BinaryNode {
-        right: Box::new(ast_stack.pop_back().unwrap()),
-        left: Box::new(ast_stack.pop_back().unwrap()),
-        op: op_stack.pop_back().unwrap(),
-    };
-    while !ast_stack.is_empty() {
-        ast = Ast::BinaryNode {
-            left: Box::new(ast_stack.pop_back().unwrap()),
-            right: Box::new(ast),
-            op: op_stack.pop_back().unwrap(),
-        }
-    }
-    return Ok(ast);
-}
 pub fn parse_expression(input: &mut InputFile) -> Result<Ast, String> {
     let t = next_token(input);
     let ast = match t.kind {
@@ -75,4 +41,39 @@ pub fn parse_let_expr(input: &mut InputFile) -> Result<Ast, String> {
         }
         e => Err(format!("Expected identifier, but got `{e:?}`"))
     }
+}
+
+#[allow(dead_code)]
+fn parse_expression_iter(input: &mut InputFile) -> Result<Ast, String> {
+    let mut ast_stack: VecDeque<Ast> = VecDeque::new();
+    let mut op_stack: VecDeque<BinaryOperator> = VecDeque::new();
+    loop {
+        let t = next_token(input);
+        match &t.kind {
+            TokenKind::Identifier(_) | TokenKind::Literal(_) => ast_stack.push_back(Ast::ValueNode(t)),
+            e => return Err(format!("Expected identifier ot literal, but got `{e:?}`")),
+        }
+        let t = next_token(input);
+        match t.kind {
+            TokenKind::Operator(OperatorKind::Semicol) => break,
+            TokenKind::Operator(OperatorKind::Binary(bin)) => op_stack.push_back(bin),
+            e => return Err(format!("Expected operator or semicolon, but got `{e:?}`")),
+        }
+    }
+    if op_stack.is_empty() {
+        return Ok(ast_stack.pop_front().unwrap());
+    }
+    let mut ast = Ast::BinaryNode {
+        right: Box::new(ast_stack.pop_back().unwrap()),
+        left: Box::new(ast_stack.pop_back().unwrap()),
+        op: op_stack.pop_back().unwrap(),
+    };
+    while !ast_stack.is_empty() {
+        ast = Ast::BinaryNode {
+            left: Box::new(ast_stack.pop_back().unwrap()),
+            right: Box::new(ast),
+            op: op_stack.pop_back().unwrap(),
+        }
+    }
+    return Ok(ast);
 }
