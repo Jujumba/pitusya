89f3c07c5b1736bcf4235fbbbbc847b536136b2a
Jujumba <far77578@gmail.com>
2023-06-08 15:55:58 +0200

    parser: add unary expressions parsing

--- src/ast/parser.rs
+++ src/ast/parser.rs
@@ -43,11 +43,24 @@ pub fn parse_expression(input: &mut InputFile) -> Result<Ast, String> {
                 }
             }
         }
+        TokenKind::Operator(op @ OperatorKind::UNot) => {
+            return Ok(Ast::UnaryNode {
+                value: Box::new(parse_expression(input)?),
+                op,
+            });
+        }
+        TokenKind::Operator(op @ OperatorKind::BWNot) => {
+            return Ok(Ast::UnaryNode {
+                value: Box::new(parse_expression(input)?),
+                op,
+            });
+        }
         e => return expect!("identifier or literal", e),
     };
     match next_token(input).kind {
-        TokenKind::Operator(OperatorKind::Semicol)
-        | TokenKind::Operator(OperatorKind::RParen) => Ok(ast),
+        TokenKind::Operator(OperatorKind::Semicol) | TokenKind::Operator(OperatorKind::RParen) => {
+            Ok(ast)
+        }
         TokenKind::Operator(op) => Ok(Ast::BinaryNode {
             left: Box::new(ast),
             right: Box::new(parse_expression(input)?),
@@ -60,12 +73,10 @@ pub fn parse_let_expr(input: &mut InputFile) -> Result<Ast, String> {
     let assignee = next_token(input);
     match &assignee.kind {
         TokenKind::Identifier(_) => match next_token(input).kind {
-            TokenKind::Operator(OperatorKind::Equals) => {
-                Ok(Ast::LetNode {
-                    assignee: Box::new(Ast::ValueNode(assignee)),
-                    value: Box::new(parse_expression(input)?),
-                })
-            }
+            TokenKind::Operator(OperatorKind::Equals) => Ok(Ast::LetNode {
+                assignee: Box::new(Ast::ValueNode(assignee)),
+                value: Box::new(parse_expression(input)?),
+            }),
             e => expect!("=", e),
         },
         e => expect!("identifier", e),
