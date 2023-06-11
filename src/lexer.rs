af03da2a6b5eda9a00cc5617139dd1317a5be365
Jujumba <far77578@gmail.com>
2023-06-11 16:38:26 +0200

    lexer: use OnceLock instead of lazy_static

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,43 +1,14 @@
 pub mod tokens;
 
-use lazy_static::lazy_static;
 use regex::Regex;
+use std::sync::OnceLock;
 use tokens::{KeywordKind, LiteralKind, NumType, OperatorKind, Token, TokenKind};
 
 use crate::input::InputFile;
 
-type Handler = dyn Fn(&str) -> TokenKind + Sync;
+type Handler = dyn Fn(&str) -> TokenKind + Sync + Send;
 
-lazy_static! {
-    static ref SPEC: Vec<(Regex, Box<Handler>)> = vec! {
-        (Regex::new(r"[0-9]+").unwrap(), Box::new(
-            |s| {
-                TokenKind::Literal(LiteralKind::Num(NumType::Int(s.parse().unwrap())))
-            }
-        )),
-        (Regex::new("\"[a-zA-Z0-0]+\"").unwrap(), Box::new(
-            |s| {
-                TokenKind::Literal(LiteralKind::Str(s.into()))
-            }
-        )),
-        (Regex::new(r"[a-zA-Z0-9]+").unwrap(), Box::new(
-            |s| {
-                match KeywordKind::try_from(s) {
-                    Ok(keyword) => TokenKind::Keyword(keyword),
-                    _ => TokenKind::Identifier(s.into())
-                }
-            }
-        )),
-        (Regex::new(r"<<=|>>=|<=|>=|\+=|\-=|\*=|/=|\|=|\^=|&=|%=|==|<<|>>|=|\+|-|\*|/|%|&|\^|\||~|!|<|>|;|\(|\)|[|]|\{|\}").unwrap(), Box::new(
-            |s| {
-                match OperatorKind::try_from(s) {
-                    Ok(operator) => TokenKind::Operator(operator),
-                    _ => TokenKind::Undefined(s.chars().next().unwrap())
-                }
-            }
-        ))
-    };
-}
+static SPEC: OnceLock<Vec<(Regex, Box<Handler>)>> = OnceLock::new();
 
 pub fn next_token(input: &mut InputFile) -> Token {
     if input.out_of_bounds() {
@@ -49,7 +20,7 @@ pub fn next_token(input: &mut InputFile) -> Token {
     input.skip_spaces();
     let content = input.get_str();
     let curs = input.get_cursor();
-    for (regex, closure) in SPEC.iter() {
+    for (regex, closure) in get_specification().iter() {
         match regex.find_at(content, curs) {
             Some(m) if m.start() == curs => {
                 let len = m.len();
@@ -66,4 +37,33 @@ pub fn next_token(input: &mut InputFile) -> Token {
         kind: TokenKind::Undefined(c),
         len: 1,
     }
+}
+
+fn get_specification<'a>() -> &'a Vec<(Regex, Box<Handler>)> {
+    SPEC.get_or_init(|| {
+        vec![
+            (
+                Regex::new(r"[0-9]+").unwrap(),
+                Box::new(|s| TokenKind::Literal(LiteralKind::Num(NumType::Int(s.parse().unwrap())))),
+            ),
+            (
+                Regex::new("\"[a-zA-Z0-0]+\"").unwrap(),
+                Box::new(|s| TokenKind::Literal(LiteralKind::Str(s.into()))),
+            ),
+            (
+                Regex::new(r"[a-zA-Z0-9]+").unwrap(),
+                Box::new(|s| match KeywordKind::try_from(s) {
+                    Ok(keyword) => TokenKind::Keyword(keyword),
+                    _ => TokenKind::Identifier(s.into()),
+                }),
+            ),
+            (
+                Regex::new(r"<<=|>>=|<=|>=|\+=|\-=|\*=|/=|\|=|\^=|&=|%=|==|<<|>>|=|\+|-|\*|/|%|&|\^|\||~|!|<|>|;|\(|\)|[|]|\{|\}").unwrap(),
+                Box::new(|s| match OperatorKind::try_from(s) {
+                    Ok(operator) => TokenKind::Operator(operator),
+                    _ => TokenKind::Undefined(s.chars().next().unwrap()),
+                }),
+            ),
+        ]
+    })
 }
