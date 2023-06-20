468af2ce46c1b518c4e2979ecbe715a12730dc3f
Jujumba <far77578@gmail.com>
2023-06-20 23:31:59 +0200

    build.rs: load LLVM version from .env file

--- build.rs
+++ build.rs
@@ -1,14 +1,19 @@
+use dotenv::dotenv;
 use std::process::Command;
 
 fn main() {
+    dotenv().expect("Missing the .env file in the source root");
+    let llvm_version = std::env::var("LLVM_VERSION").expect("Specify the LLVM version to link!");
+
+    println!("cargo:rustc-link-lib=LLVM-{}", llvm_version);
+    println!("cargo:rerun-if-changed=src/c/pitusya_llvm_wrapper.c");
+
     let llvm_flags_process = Command::new("llvm-config")
         .args(["--cflags", "--ldflags", "--system-libs", "--libs", "core"])
         .output()
         .expect("Build error: LLVM is not installed!");
     let llvm_flags = String::from_utf8(llvm_flags_process.stdout).unwrap();
 
-    println!("cargo:rerun-if-changed=src/c/pitusya_llvm_wrapper.c");
-    println!("cargo:rustc-link-lib=LLVM-16");
     cc::Build::new()
         .compiler("clang")
         .flag("-c")
