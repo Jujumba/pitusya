851e6598d0d356809e01a8fb55fa784fb98433c5
Jujumba <far77578@gmail.com>
2023-06-23 15:15:38 +0200

    wrapper: bug fix: don't wrap each expression in a block

--- src/c/pitusya_llvm_wrapper.c
+++ src/c/pitusya_llvm_wrapper.c
@@ -1,10 +1,9 @@
 #include <llvm-c/Types.h>
+#include <llvm-c/Target.h>
+#include <llvm-c/Analysis.h>
+#include "llvm-c/Core.h"
 #include <stdio.h>
 #include <string.h>
-#include <llvm-c/Target.h>
-#include "llvm-c/Core.h"
-
-typedef LLVMValueRef(*LLVMFunction)(LLVMBuilderRef, LLVMValueRef,LLVMValueRef, const char*);
 
 LLVMContextRef CONTEXT = NULL;
 LLVMModuleRef MODULE = NULL;
@@ -21,11 +20,12 @@ void PITUSYAPostDestroy() {
     LLVMDisposeModule(MODULE);
     LLVMContextDispose(CONTEXT);
 }
-static LLVMValueRef PITUSYAGetAnonExpr(LLVMFunction p, LLVMValueRef lhs, LLVMValueRef rhs, const char* name) {
+LLVMValueRef PITUSYAWrapInAnonExpr(LLVMValueRef v) {
     LLVMValueRef anon = LLVMAddFunction(MODULE, "__anon_expr", LLVMFunctionType(LLVMDoubleTypeInContext(CONTEXT), NULL, 0, 0));
     LLVMBasicBlockRef entryBlock = LLVMAppendBasicBlockInContext(CONTEXT, anon, "entry");
     LLVMPositionBuilderAtEnd(BUILDER, entryBlock);
-    LLVMBuildRet(BUILDER, p(BUILDER, lhs, rhs, name));
+    LLVMBuildRet(BUILDER, v);
+    LLVMVerifyFunction(anon, LLVMAbortProcessAction);
     return anon;
 }
 LLVMValueRef PITUSYAGenerateFP(double n) {
@@ -35,16 +35,16 @@ LLVMValueRef PITUSYAGenerateString(char* s, size_t len) {
     return LLVMConstString(s, len, 0);
 }
 LLVMValueRef PITUSYABuildAdd(LLVMValueRef lhs, LLVMValueRef rhs) {
-    return PITUSYAGetAnonExpr(LLVMBuildFAdd, lhs, rhs, "addtmp");
+    return LLVMBuildFAdd(BUILDER, lhs, rhs, "addtmp");
 }
 LLVMValueRef PITUSYABuildMul(LLVMValueRef lhs, LLVMValueRef rhs) {
-    return PITUSYAGetAnonExpr(LLVMBuildFMul, lhs, rhs, "multmp");
+    return LLVMBuildFMul(BUILDER, lhs, rhs, "multmp");
 }
 LLVMValueRef PITUSYABuildSub(LLVMValueRef lhs, LLVMValueRef rhs) {
-    return PITUSYAGetAnonExpr(LLVMBuildSub, lhs, rhs, "subtmp");
+    return LLVMBuildSub(BUILDER, lhs, rhs, "subtmp");
 }
 LLVMValueRef PITUSYABuildDiv(LLVMValueRef lhs, LLVMValueRef rhs) {
-    return PITUSYAGetAnonExpr(LLVMBuildFDiv, lhs, rhs, "divtmp");
+    return LLVMBuildFDiv(BUILDER, lhs, rhs, "divtmp");
 }
 void PITUSYAPrintIR(LLVMValueRef ir) {
     char* s = LLVMPrintValueToString(ir);
