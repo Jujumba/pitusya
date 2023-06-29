b23ee6312e0e6b0b69b8d90a69b54398f56fc6bd
Jujumba <far77578@gmail.com>
2023-06-29 21:30:21 +0200

    c bindings: fix visibility

--- src/rust/codegen/bindings.rs
+++ src/rust/codegen/bindings.rs
@@ -3,14 +3,14 @@ use libc::c_void;
 pub(super) type LLVMPointer = *mut c_void;
 
 extern "C" {
-    pub(crate) fn PITUSYAPreInit();
-    pub(crate) fn PITUSYAPostDestroy();
-    pub(super) fn PITUSYACreateFunction(name: *const i8, argv: *const *const i8, argc: usize) -> LLVMPointer;
-    pub(super) fn PITUSYACreateVar(value: LLVMPointer, name: *const i8) -> LLVMPointer;
-    pub(super) fn PITUSYABuildRet(v: LLVMPointer) -> LLVMPointer;
-    pub(super) fn PITUSYAGenerateFP(n: f64) -> LLVMPointer;
-    pub(super) fn PITUSYABuildAdd(lhs: LLVMPointer, rhs: LLVMPointer) -> LLVMPointer;
-    pub(super) fn PITUSYABuildMul(lhs: LLVMPointer, rhs: LLVMPointer) -> LLVMPointer;
-    pub(super) fn PITUSYABuildSub(lhs: LLVMPointer, rhs: LLVMPointer) -> LLVMPointer;
-    pub(super) fn PITUSYABuildDiv(lhs: LLVMPointer, rhs: LLVMPointer) -> LLVMPointer;
+    pub fn PITUSYAPreInit();
+    pub fn PITUSYAPostDestroy();
+    pub fn PITUSYACreateFunction(name: *const i8, argv: *const *const i8, argc: usize) -> LLVMPointer;
+    pub fn PITUSYACreateVar(value: LLVMPointer, name: *const i8) -> LLVMPointer;
+    pub fn PITUSYABuildRet(v: LLVMPointer) -> LLVMPointer;
+    pub fn PITUSYAGenerateFP(n: f64) -> LLVMPointer;
+    pub fn PITUSYABuildAdd(lhs: LLVMPointer, rhs: LLVMPointer) -> LLVMPointer;
+    pub fn PITUSYABuildMul(lhs: LLVMPointer, rhs: LLVMPointer) -> LLVMPointer;
+    pub fn PITUSYABuildSub(lhs: LLVMPointer, rhs: LLVMPointer) -> LLVMPointer;
+    pub fn PITUSYABuildDiv(lhs: LLVMPointer, rhs: LLVMPointer) -> LLVMPointer;
 }
