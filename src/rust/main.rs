d0c6731160f40ac4c25979417d179bf4dba67fba
Jujumba <far77578@gmail.com>
2023-07-06 18:26:26 +0200

    codegen: rename struct and refactor

--- src/rust/main.rs
+++ src/rust/main.rs
@@ -2,7 +2,7 @@ use pitusya::*;
 
 fn main() {
     let mut titi = input::InputFile::new(String::from("fn bibix(a) {let b = a + 1; ret b + 2;}"));
-    let cc = codegen::Codegenerator::default();
+    let cc = codegen::Cg::default();
     let asts = pitusya::ast::parser::parse(&mut titi);
     asts.into_iter().for_each(|ast| cc.codegen(ast));
 }
