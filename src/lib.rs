b1cde86a778f16bbaf578d1b727f2b56cf31461b
Jujumba <far77578@gmail.com>
2023-08-17 22:02:22 +0200

    lib: abort: add empty macro invocation

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,9 @@ pub mod pass;
 
 #[macro_export]
 macro_rules! abort {
+    () => {
+        ::std::process::exit(0x_48_41_50_50_59_42_44_41_59_i128 as i32)
+    };
     ($msg:expr) => {{
         eprintln!($msg);
         ::std::process::exit(0x_48_41_50_50_59_42_44_41_59_i128 as i32)
@@ -34,29 +37,29 @@ macro_rules! abort_if_not {
 #[cfg(test)]
 mod tests {
     use crate::ast::{parser, Ast};
-    use crate::input::InputFile;
+    use crate::input::CursoredFile;
     use crate::lexer::next_token;
     use crate::lexer::tokens::*;
     #[test]
     #[ignore = "Compiler doesn't panic anymore, instead it exits with a non-zero value (what is untestable)."]
     fn test_bad_input() {
-        let mut bad = InputFile::new("1 = 1;".to_string());
+        let mut bad = CursoredFile::new("1 = 1;".to_string());
         parser::parse(&mut bad);
     }
     #[test]
     fn test_let_expr() {
-        let mut parse = InputFile::new(
+        let mut parse = CursoredFile::new(
             "fn main() {
             let pitusya = \"cool\";
         }"
-            .to_string()
+            .to_string(),
         );
         parser::parse(&mut parse);
     }
     #[test]
     #[ignore = "I've added functions and user is enforced to write their code in them"]
     fn test_lexer() {
-        let mut tok_seq = InputFile::new(String::from("==<=|"));
+        let mut tok_seq = CursoredFile::new(String::from("==<=|"));
         assert_eq!(
             next_token(&mut tok_seq).kind,
             TokenKind::Operator(OperatorKind::Binary(BinaryOperatorKind::Comparision))
@@ -70,7 +73,7 @@ mod tests {
     #[test]
     #[ignore = "Okey, now all my tests are useless"]
     fn test_while_expression_parsing() {
-        let mut input = InputFile::new(String::from(
+        let mut input = CursoredFile::new(String::from(
             "while 1 == 1; {
             let hello = \"world\";
         }",
@@ -81,7 +84,7 @@ mod tests {
     #[test]
     #[ignore = "Even though my parser is testable, it would be so tedious to write tests further"]
     fn test_if_expression_parsing() {
-        let mut input = InputFile::new(String::from(
+        let mut input = CursoredFile::new(String::from(
             "if 1 == 2; {
                 let wow = \"uWu\";
             }",
